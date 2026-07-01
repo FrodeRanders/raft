14fdbde203c4b63d05ef971a31491b4e76082937
Frode Randers <frode.randers@gmail.com>
2026-07-01 11:28:04 +0200

    graft-proto: package proto file locally for crates.io
    The proto file lived in ../../raft-wire/ which is outside the crate
    directory and was not packaged by cargo publish. Copy it into
    proto/ within the crate and update build.rs accordingly.

--- graft-rust/graft-proto/build.rs
+++ graft-rust/graft-proto/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     prost_build::compile_protos(
-        &["../../raft-wire/src/main/proto/raft.proto"],
-        &["../../raft-wire/src/main/proto/"],
+        &["proto/raft.proto"],
+        &["proto/"],
     )
     .unwrap();
 }
